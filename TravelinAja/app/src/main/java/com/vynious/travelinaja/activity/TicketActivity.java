9efd66ec1d9d549505756ef74a4b8fb06a35b580
agungauliam208 <agung.aulia@widyatama.ac.id>
2024-07-16 16:49:10 +0700

    Initial commit

---
+++ TravelinAja/app/src/main/java/com/vynious/travelinaja/activity/TicketActivity.java
@@ -0,0 +1,67 @@
+package com.vynious.travelinaja.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.vynious.travelinaja.R;
+import com.vynious.travelinaja.databinding.ActivityTicketBinding;
+import com.vynious.travelinaja.domain.ItemDomain;
+
+public class TicketActivity extends BaseActivity {
+    ActivityTicketBinding binding;
+    private ItemDomain object;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityTicketBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        setVariable();
+
+    }
+
+    private void setVariable() {
+        Glide.with(TicketActivity.this)
+                .load(object.getPic())
+                .into(binding.pic);
+
+        Glide.with(TicketActivity.this)
+                .load(object.getTourGuidePic())
+                .into(binding.profile);
+
+        binding.backBtn.setOnClickListener(v ->finish());
+        binding.titleTxt.setText(object.getTitle());
+        binding.durationTxt.setText(object.getDuration());
+        binding.tourGuideTxt.setText(object.getDateTour());
+        binding.timeTxt.setText(object.getTimeTour());
+        binding.tourGuideNameTxt.setText(object.getTourGuideName());
+
+        binding.callBtn.setOnClickListener(view -> {
+            Intent sendIntent=new Intent(Intent.ACTION_VIEW);
+            sendIntent.setData(Uri.parse("sms:"+object.getTourGuidePhone()));
+            sendIntent.putExtra("sms_body", "type your message");
+            startActivity(sendIntent);
+        });
+
+        binding.messageBtn.setOnClickListener(view -> {
+            String phone=object.getTourGuidePhone();
+            Intent intent=new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+            startActivity(intent);
+        });
+    }
+
+    private void getIntentExtra() {
+        object = (ItemDomain) getIntent().getSerializableExtra("object");
+    }
+}
---
+++ TravelinAja/app/src/main/java/com/vynious/travelinaja/domain/SliderItems.java
@@ -0,0 +1,16 @@
+package com.vynious.travelinaja.domain;
+
+public class SliderItems {
+    private String url;
+
+    public SliderItems() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
